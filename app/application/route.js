c9f9637b6511ef6bdfb49a18a588e2e9c98c6fcd
edborden <borden.edward@gmail.com>
2015-12-17 18:05:12 -0500

    session is persisted and reloaded

--- app/application/route.js
+++ app/application/route.js
@@ -1,24 +1,78 @@
 import Ember from 'ember';
+const {
+  isEmpty,
+  inject: { service }
+} = Ember;
 
 export default Ember.Route.extend({
 
-  // EVENTS
+  // services
+  meService: service('me'),
 
+  // events
   beforeModel() {
-    return this.get('session').fetch().then(() => {
-      console.log('success session');
-    }, () => {
-      console.log('no session');
+    let session = this.get('session');
+    return session.fetch().then(() => {
+      this._authenticate();
+    })
+    .catch( (error)=> {
+      console.log('no session', error);
     });
   },
 
-  // ACTIONS
-
+  // actions
   actions: {
 
     signOut() {
       this.get('session').close();
+    },
+
+    authenticate() {
+      this._authenticate();
     }
 
+  },
+
+  _authenticate() {
+    let store = this.get('store');
+    let meService = this.get('meService');
+    let session = this.get('session');
+    let currentUser = session.get('currentUser');
+    let { cachedUserProfile } = currentUser;
+    store.query('facebook', {
+      orderBy: 'uid',
+      equalTo: currentUser.id
+    })
+    .then((facebook) => {
+      if (isEmpty(facebook)) {
+        let newUser = store.createRecord('user', {
+          // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
+          firstName: cachedUserProfile.first_name,
+          lastName: cachedUserProfile.last_name
+        });
+        let newFacebook = store.createRecord('facebook', {
+          token: currentUser.accessToken,
+          uid: currentUser.id,
+          user: newUser
+        });
+        newUser.set('facebook', newFacebook);
+        newUser.save();
+        newFacebook.save();
+      } else {
+        console.log('Facebook exists');
+
+        facebook.get('firstObject').get('user')
+        .then((currentUser) => {
+          console.log(currentUser);
+          if (isEmpty(currentUser)) {
+            console.log('No user');
+          } else {
+            meService.set('model', currentUser);
+            this.transitionTo('index');
+          }
+        });
+      }
+
+    });
   }
 });
--- app/login/route.js
+++ app/login/route.js
@@ -1,64 +1,20 @@
 import Ember from 'ember';
 const {
-  Route,
-  isEmpty,
-  inject: { service }
+  Route
 } = Ember;
 
 export default Route.extend({
-
-  // services
-  meService: service('me'),
 
   // actions
   actions: {
 
     signIn(provider) {
-      let store = this.get('store');
       let session = this.get('session');
-      let meService = this.get('meService');
       session.open('firebase', { provider })
-      .then((data) => {
-        console.log(data);
-        let {
-          currentUser,
-          currentUser: { cachedUserProfile }
-        } = data;
-        store.query('facebook', {
-          orderBy: 'uid',
-          equalTo: currentUser.id
-        }).then((facebook) => {
-          if (isEmpty(facebook)) {
-            let newUser = store.createRecord('user', {
-              // jscs:disable requireCamelCaseOrUpperCaseIdentifiers
-              firstName: cachedUserProfile.first_name,
-              lastName: cachedUserProfile.last_name
-            });
-            let newFacebook = store.createRecord('facebook', {
-              token: currentUser.accessToken,
-              uid: currentUser.id,
-              user: newUser
-            });
-            newUser.set('facebook', newFacebook);
-            newUser.save();
-            newFacebook.save();
-          } else {
-            console.log('Facebook exists');
-
-            facebook.get('firstObject').get('user').then((currentUser) => {
-              console.log(currentUser);
-              if (isEmpty(currentUser)) {
-                console.log('No user');
-              } else {
-                meService.set('model', currentUser);
-                this.transitionTo('index');
-              }
-            });
-
-          }
-        });
-        return false;
+      .then(() => {
+        this.send('authenticate');
       });
+      return false;
     }
 
   }
