c00075cc84e0722cc60bd9762297a2f14adb1b1a
edborden <borden.edward@gmail.com>
2016-01-09 09:36:18 -0500

    detail oncreate comment

--- app/models/detail.js
+++ app/models/detail.js
@@ -28,6 +28,12 @@ export default Model.extend({
   experiences: hasMany('experience'),
 
   // events
+  /* 
+    we can't save an empty detail to firebase, there has to be some content,
+    so the detail will get created with the first att/association that gets
+    added to it. that model won't know about this model's status, so we use
+    this hook to automatically add the detail to the user on creation.
+  */
   didCreate() {
     let me = this.get('me');
     me.set('detail', this);
