9aa7e9defb55b4a50906cad46b64deca8c5f18f6
edborden <borden.edward@gmail.com>
2016-01-12 14:23:27 -0500

    can remove attributes from preferences detail

--- app/me/preferences/controller.js
+++ app/me/preferences/controller.js
@@ -12,6 +12,52 @@ export default Controller.extend({
 
   // actions
   actions: {
+    removeAge() {
+      let detail = this.get('model');
+      detail.set('age', null);
+      detail.save();
+    },
+
+    removeEducation(education) {
+      let detail = this.get('model');
+      let educations = detail.get('educations');
+      educations.removeObject(education);
+      detail.save();
+      this.get('notify').success(`Successfully removed ${education.get('degree').get('levelString')} degree.`);
+    },
+
+    removeLanguage(language) {
+      let detail = this.get('model');
+      let languages = detail.get('languages');
+      languages.removeObject(language);
+      detail.save();
+      this.get('notify').success(`Successfully removed ${language.get('tongue').get('name')} language.`);
+    },
+
+    removeExperience(experience) {
+      let detail = this.get('model');
+      let experiences = detail.get('experiences');
+      experiences.removeObject(experience);
+      detail.save();
+      this.get('notify').success(`Successfully removed ${experience.get('industry').get('name')} experience.`);
+    },
+
+    removeSpecialization(specialization) {
+      let detail = this.get('model');
+      let specializations = detail.get('specializations');
+      specializations.removeObject(specialization);
+      detail.save();
+      this.get('notify').success(`Successfully removed ${specialization.get('name')} specialization.`);
+    },
+
+    removeInterest(interest) {
+      let detail = this.get('model');
+      let interests = detail.get('interests');
+      interests.removeObject(interest);
+      detail.save();
+      this.get('notify').success(`Successfully removed ${interest.get('name')} interest.`);
+    },
+
     removeCountry(country) {
       let detail = this.get('model');
       let countries = detail.get('countries');
