3a19280de1a64073b2039ebfc921bdd4dadcc848
edborden <borden.edward@gmail.com>
2016-01-14 08:53:42 -0500

    user gender moved in gender model

---
+++ app/models/gender.js
@@ -0,0 +1,25 @@
+import DS from 'ember-data';
+
+const {
+  Model,
+  attr
+} = DS;
+
+export default Model.extend({
+
+  // attributes
+  gender: attr('number'),
+  genderEnum: {
+    0: 'female',
+    1: 'male'
+  },
+
+  // computed
+  @computed('gender')
+  genderString() {
+    let gender = this.get('gender');
+    let genderEnum = this.get('genderEnum');
+    return genderEnum[gender];
+  }
+
+});
--- app/models/user.js
+++ app/models/user.js
@@ -14,23 +14,11 @@ export default Model.extend({
   firstName: attr('string'),
   lastName: attr('string'),
   email: attr('string'),
-  gender: attr('number'),
-  genderEnum: {
-    0: 'female',
-    1: 'male'
-  },
 
   // associations
   facebook: belongsTo('facebook'),
   detail: belongsTo('detail'),
   listings: hasMany('listing'),
-
-  // computed
-  @computed('gender')
-  genderString() {
-    let gender = this.get('gender');
-    let genderEnum = this.get('genderEnum');
-    return genderEnum[gender];
-  }
+  gender: belongsTo('gender')
 
 });
