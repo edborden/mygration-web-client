fa1f21aa7d0b9be828c2b21ce624a1437e508b36
edborden <borden.edward@gmail.com>
2016-01-14 12:32:33 -0500

    listing-editor

---
+++ app/components/listing-editor/component.js
@@ -0,0 +1,57 @@
+import Ember from 'ember';
+import { alias } from 'ember-computed-decorators';
+import HasMe from 'mygration-web-client/mixins/has-me';
+import EmberValidations from 'ember-validations';
+
+const {
+  Component,
+  inject: { service }
+} = Ember;
+
+export default Component.extend(HasMe, EmberValidations, {
+
+  // services
+  routing: service('-routing'),
+  notify: service(),
+
+  // computed
+  @alias('model.title') title,
+  @alias('model.description') description,
+  @alias('model.price') price,
+
+  // validations
+  validations: {
+    title: {
+      presence: true
+    },
+    description: {
+      presence: true
+    },
+    price: {
+      presence: true
+    }
+  },
+
+  // actions
+  actions: {
+    async save() {
+      let model = this.get('model');
+      let isNew = model.get('isNew');
+      await model.save();
+      if (isNew) {
+        this.addToUser(model);
+      }
+      this.get('routing').transitionTo('me.listings');
+      this.get('notify').success(`Successfully saved listing.`);
+    }
+  },
+
+  // helpers
+  addToUser(model) {
+    let me = this.get('me');
+    let listings = me.get('listings');
+    listings.pushObject(model);
+    me.save();
+  }
+
+});
