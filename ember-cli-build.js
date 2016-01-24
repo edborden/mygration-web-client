2b489d805118ece4a6b8739e01e57eaa11808c51
edborden <borden.edward@gmail.com>
2016-01-24 10:23:24 -0500

    remove font-awesome

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -19,32 +19,11 @@ module.exports = function(defaults) {
   var pickFiles = require('broccoli-funnel');
   var mergeTrees = require('broccoli-merge-trees');
 
-  // Font-Awesome
-  var fontAwesomeFonts = pickFiles('bower_components/components-font-awesome/fonts', {
-    destDir: '/fonts'
+  // Materialize
+  var materializeFonts = pickFiles('bower_components/Materialize/font/roboto', {
+    destDir: '/font/roboto'
   });
-
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Thin.woff2', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Thin.woff', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Thin.ttf', { destDir: 'font/roboto' });
-
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Light.woff2', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Light.woff', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Light.ttf', { destDir: 'font/roboto' });
-
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Regular.woff2', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Regular.woff', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Regular.ttf', { destDir: 'font/roboto' });
-
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Medium.woff2', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Medium.woff', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Medium.ttf', { destDir: 'font/roboto' });
-
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Bold.woff2', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Bold.woff', { destDir: 'font/roboto' });
-  app.import('bower_components/Materialize/dist/font/roboto/Roboto-Bold.ttf', { destDir: 'font/roboto' });
-
   app.import('bower_components/Materialize/dist/js/materialize.js');  
 
-  return mergeTrees([app.toTree(),fontAwesomeFonts]);
+  return mergeTrees([ app.toTree(), materializeFonts ]);
 };
