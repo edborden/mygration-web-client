69231368ea5d8523f7d4bf4cb250375b83e49a6b
edborden <borden.edward@gmail.com>
2016-01-27 13:50:43 -0500

    extraneous import

--- app/me/listings/add/route.js
+++ app/me/listings/add/route.js
@@ -3,13 +3,29 @@ import HasMe from 'mygration-web-client/mixins/has-me';
 
 const {
   Route,
-  isEmpty
+  isBlank,
+  inject: { service }
 } = Ember;
 
 export default Route.extend(HasMe, {
 
+  // services
+  notify: service(),
+
+  // events
+  beforeModel() {
+    const profile = this.get('me').get('profile');
+    if (isBlank(profile)) {
+      this.transitionTo('me.profile');
+      this.get('notify').success('You must first fill out your profile before creating a listing.');
+    }
+  },
+
   model() {
-    return this.get('store').createRecord('listing');
+    const me = this.get('me');
+    const listing = this.get('store').createRecord('listing');
+    listing.set('user', me);
+    return listing;
   }
 
 });
